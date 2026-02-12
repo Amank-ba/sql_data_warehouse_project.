716194fbd4c143ab6c86aff78f1e461a96269a41
Aman Kumar Singh <rajputamansingh9838@gmail.com>
2026-02-12 22:52:30 +0530

    quality_checks_gold.sql

---
+++ tests/quality_checks_gold.sql
@@ -0,0 +1,64 @@
+/*
+===============================================================================
+Quality Checks
+===============================================================================
+Script Purpose:
+    This script performs quality checks to validate the integrity, consistency, 
+    and accuracy of the Gold Layer. These checks ensure:
+    - Uniqueness of surrogate keys in dimension tables.
+    - Referential integrity between fact and dimension tables.
+    - Validation of relationships in the data model for analytical purposes.
+
+Usage Notes:
+    - Investigate and resolve any discrepancies found during the checks.
+===============================================================================
+*/
+
+-- ====================================================================
+-- Checking 'gold.dim_customers'
+-- ====================================================================
+-- Check for Uniqueness of Customer Key in gold.dim_customers
+-- Expectation: No results 
+
+DROP VIEW IF EXISTS qc_dim_customers_duplicates;
+
+CREATE VIEW qc_dim_customers_duplicates AS
+SELECT 
+    customer_key,
+    COUNT(*) AS duplicate_count
+FROM gold_dim_customers
+GROUP BY customer_key
+HAVING COUNT(*) > 1;
+
+-- ====================================================================
+-- Checking 'gold.dim_products'
+-- ====================================================================
+-- Check for Uniqueness of Product Key in gold.dim_products
+-- Expectation: No results
+DROP VIEW IF EXISTS qc_dim_products_duplicates;
+
+CREATE VIEW qc_dim_products_duplicates AS
+SELECT 
+    product_key,
+    COUNT(*) AS duplicate_count
+FROM gold_dim_products
+GROUP BY product_key
+HAVING COUNT(*) > 1;
+
+-- ====================================================================
+-- Checking 'gold.fact_sales'
+-- ====================================================================
+-- Check the data model connectivity between fact and dimensions
+DROP VIEW IF EXISTS qc_fact_sales_connectivity;
+
+CREATE VIEW qc_fact_sales_connectivity AS
+SELECT 
+    f.*,
+    c.customer_key,
+    p.product_key
+FROM gold_fact_sales f
+LEFT JOIN gold_dim_customers c
+    ON c.customer_key = f.customer_key
+LEFT JOIN gold_dim_products p
+    ON p.product_key = f.product_key
+WHERE p.product_key IS NULL OR c.customer_key IS NULL;
